39672f0485f66d0f94c83b07ae8b1f32bef54b3d
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-01-03 19:59:07 +0530

    feat: middlewares setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Listening on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("MONOG db connection failed!!! ", error);
+})
 
 
 
